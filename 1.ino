893c2386a350ff26146e23133e60d6ffdd8272fd
Kishandharan <152014111+Kishandharan@users.noreply.github.com>
2024-05-04 20:58:03 +0530

    Update 1.ino

--- 1.ino
+++ 1.ino
@@ -2,14 +2,14 @@ void(* reset)(void) = 0; //Creating a function to reset the Arduino board
 
 void setup()
 {
-  Serial.begin(9600);            //Initializing Serial communication
+  Serial.begin(9600); //Initializing Serial communication
   Serial.println("Starting..."); //Printing "Start..." in the Serial monitor
-  delay(1000);                   //Delaying 1000 milliseconds (one second)
+  delay(1000); //Delaying 1000 milliseconds (one second)
 }
 
 void loop()
 {
   Serial.println("Resetting..."); //Printing "Resetting" to the Serial monitor
-  delay(1000); 					          //Delaying 1000 milliseconds (one second) 
-  reset();                        //Calling the "reset" function to reset the Arduino board
+  delay(1000); //Delaying 1000 milliseconds (one second) 
+  reset(); //Calling the "reset" function to reset the Arduino board
 }
